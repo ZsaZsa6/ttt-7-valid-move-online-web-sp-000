f1cb8a93f11e49b017a9f86d566f247e926cd55b
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-25 14:36:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
 # code your #valid_move? method here
+<<<<<<< HEAD
 def position_taken?(board, index)
   if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
     false
@@ -11,5 +12,14 @@ def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0,8)
     true
     
+=======
+def valid_move?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil && board[index] == (0..8)
+      true 
+  if (board[index] == "X") || (board[index] == "O")
+     false
+  
+    end 
+>>>>>>> 4601cfffd17ab7821003904bba013ef97738e135
   end
 end
